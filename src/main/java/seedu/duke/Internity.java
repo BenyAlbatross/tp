3b43f49913b681b81e7cdc33766977f1a7cf1a97
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-01 19:37:39 +0800

    Add basic functionality to main
    Added while loop that takes in input until bye is entered.
    
    Also renamed Duke class to Internity.

---
+++ src/main/java/seedu/duke/Internity.java
@@ -0,0 +1,34 @@
+package seedu.duke;
+
+import java.util.Scanner;
+
+public class Internity {
+    public static void initInternity() {
+        String logo = " ___       _                  _ _         \n" +
+                "|_ _|_ __ | |_ ___ _ __ _ __ (_) |_ _   _ \n" +
+                " | || '_ \\| __/ _ \\ '__| '_ \\| | __| | | |\n" +
+                " | || | | | ||  __/ |  | | | | | |_| |_| |\n" +
+                "|___|_| |_|\\__\\___|_|  |_| |_|_|\\__|\\__, |\n" +
+                "                                    |___/ ";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What is your name?");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Hello " + in.nextLine());
+        boolean isExit = false;
+        while (!isExit) {
+            String s = in.nextLine();
+            if (s.equals("bye")) {
+                isExit = true;
+            }
+        }
+    }
+
+
+    /**
+     * Main entry-point for the java.duke.Internity application.
+     */
+    public static void main(String[] args) {
+        initInternity();
+    }
+}
