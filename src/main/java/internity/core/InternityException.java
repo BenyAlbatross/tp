f8ae8b7a0d99f68140c600c53f8e5d05a0d304c6
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-04 19:06:52 +0800

    Update InternityException.java

--- src/main/java/internity/core/InternityException.java
+++ src/main/java/internity/core/InternityException.java
@@ -1,5 +1,13 @@
 package internity.core;
 
+/**
+ * Represents custom exceptions used throughout the Internity application.
+ *
+ * <p>
+ * This class extends {@code Exception} and provides static methods that create a specific
+ * exception message for common error conditions such as invalid input or unknown commands.
+ * </p>
+ */
 public class InternityException extends Exception {
     public InternityException(String message) {
         super(message);
