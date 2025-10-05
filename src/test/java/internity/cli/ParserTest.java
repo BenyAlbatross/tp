540bdf8b8383134b27e7b1c4506cea95272c7f8a
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-05 22:56:27 +0800

    Update ParserTest.java

--- src/test/java/internity/cli/ParserTest.java
+++ src/test/java/internity/cli/ParserTest.java
@@ -1,6 +1,8 @@
 package internity.cli;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
