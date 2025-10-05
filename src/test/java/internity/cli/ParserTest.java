e982571e25610deca3cb3a7a383a62ec6b61e055
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-05 22:45:54 +0800

    Add JUnit tests for Parser and ExitCommand

---
+++ src/test/java/internity/cli/ParserTest.java
@@ -0,0 +1,27 @@
+package internity.cli;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import internity.commands.Command;
+import internity.commands.ExitCommand;
+import internity.core.InternityException;
+
+class ParserTest {
+    @Test
+    void parseInput_unknownCommand_throwException() {
+        Parser parser = new Parser();
+        InternityException exception = assertThrows(InternityException.class, () -> {
+            parser.parseInput("Killer Queen");
+        });
+        assertEquals("Unknown command: killer", exception.getMessage());
+    }
+
+    @Test
+    void parseInput_ExitCommand_returnsExitCommand() throws InternityException {
+        Parser parser = new Parser();
+        Command command = parser.parseInput("exit");
+        assertInstanceOf(ExitCommand.class, command);
+    }
+}
---
+++ src/test/java/internity/commands/ExitCommandTest.java
@@ -0,0 +1,33 @@
+package internity.commands;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+class ExitCommandTest {
+
+    @Test
+    void execute_printsExitMessage() {
+        ExitCommand exitCommand = new ExitCommand();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+
+        try {
+            exitCommand.execute();
+            assertEquals("Thank you for using Internity! Goodbye!" + System.lineSeparator(),
+                    outContent.toString());
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
+
+    @Test
+    void isExit_returnsTrue() {
+        ExitCommand exitCommand = new ExitCommand();
+        assertTrue(exitCommand.isExit());
+    }
+}
