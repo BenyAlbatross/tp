ad64a5557d470b2ae071808d0c35d81d2d82c1aa
Vito Benedict Halim <vitobenedicthalim@gmail.com>
2025-10-08 13:53:40 +0800

    remove redundant comments

--- src/main/java/internity/core/InternshipList.java
+++ src/main/java/internity/core/InternshipList.java
@@ -5,33 +5,27 @@ import java.util.ArrayList;
 public class InternshipList {
     private final ArrayList<Internship> List;
 
-    // constructor
     public InternshipList() {
         List = new ArrayList<>();
     }
 
-    // add
     public void add(Internship item) {
         List.add(item);
     }
 
-    // delete
     public void delete(Internship item) {
         List.remove(item);
     }
 
-    // get an internship by index
     public Internship get(int index){
         return List.get(index);
     }
 
-    // list all
     public void listAll() {
         // ADD CODE HERE
         System.out.println("<PRINT ALL INTERNSHIPS>");
     }
 
-    // update internship by index
     public void update(int index) {
         // ADD CODE HERE
         Internship updateThis = List.get(index);
