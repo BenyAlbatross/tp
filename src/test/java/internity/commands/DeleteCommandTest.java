2bb1a56e65846bb48cb0c627f59515c231452f41
BenyAlbatross <chek.benjamin@gmail.com>
2025-10-10 16:59:03 +0800

    Resolve checkstyle errors

--- src/test/java/internity/commands/DeleteCommandTest.java
+++ src/test/java/internity/commands/DeleteCommandTest.java
@@ -7,7 +7,9 @@ import internity.core.Date;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class DeleteCommandTest {
 
