197b3189c0fd7c01ec33452c3effe79cf424e074
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-11 15:09:18 +0800

    Create ArgumentParser.java
    Created ArgumentParser
    ArgumentParser provides static methods that parses the arguments for each command.

---
+++ src/main/java/internity/cli/ArgumentParser.java
@@ -0,0 +1,42 @@
+package internity.cli;
+
+import internity.commands.DeleteCommand;
+import internity.commands.UpdateCommand;
+import internity.core.InternityException;
+
+public final class ArgumentParser {
+    private ArgumentParser() {} // prevent instantiation
+
+    public static DeleteCommand parseDeleteCommandArgs(String args) throws InternityException {
+        if (args == null || args.isBlank()) {
+            throw InternityException.invalidDeleteCommand();
+        }
+        try {
+            int oneBasedIndex = Integer.parseInt(args.trim());
+
+            if (oneBasedIndex <= 0) {
+                throw new InternityException("Index must be a positive integer.");
+            }
+
+            int zeroBasedIndex = oneBasedIndex - 1;
+
+            return new DeleteCommand(zeroBasedIndex);
+
+        } catch (NumberFormatException e) {
+            throw InternityException.invalidInternshipIndex();
+        }
+    }
+
+    public static UpdateCommand parseUpdateCommandArgs(String args) throws InternityException {
+        String newStatus;
+        int zeroBasedIndex;
+        try {
+            String[] splitArgs = args.split("\\s+status/");
+            zeroBasedIndex = Integer.parseInt(splitArgs[0].trim()) - 1;
+            newStatus = splitArgs[1].trim();
+        } catch (Exception e) {
+            throw new InternityException("Invalid update command format. Use: update INDEX status/NEW_STATUS");
+        }
+        return new UpdateCommand(zeroBasedIndex, newStatus);
+    }
+}
