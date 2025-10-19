bb12de616f5bd629749216317573bb617ccbefc4
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-19 19:07:37 +0800

    Create UsernameCommand.java

---
+++ src/main/java/internity/commands/UsernameCommand.java
@@ -0,0 +1,13 @@
+package internity.commands;
+
+public class UsernameCommand extends Command {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isExit() {
+        return false;
+    }
+}
