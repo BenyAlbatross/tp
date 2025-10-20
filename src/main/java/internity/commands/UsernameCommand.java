b8a76d255b43f71dfe0ac9e185dd138b72b9db7a
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-20 17:53:07 +0800

    Update UsernameCommand.java
    Created UsernameCommand

--- src/main/java/internity/commands/UsernameCommand.java
+++ src/main/java/internity/commands/UsernameCommand.java
@@ -1,9 +1,17 @@
 package internity.commands;
 
+import internity.core.InternshipList;
+
 public class UsernameCommand extends Command {
+    private String username;
+
+    public UsernameCommand(String args) {
+        username = args;
+    }
+
     @Override
     public void execute() {
-
+        InternshipList.setUsername(username);
     }
 
     @Override
