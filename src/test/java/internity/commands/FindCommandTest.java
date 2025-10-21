2d50405eef52b5ab95ade5cc95af15e188e6fc7b
BestBearrr <66015935+BestBearrr@users.noreply.github.com>
2025-10-21 23:35:48 +0800

    Remove unused import in FindCommandTest

--- src/test/java/internity/commands/FindCommandTest.java
+++ src/test/java/internity/commands/FindCommandTest.java
@@ -2,7 +2,6 @@ package internity.commands;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import internity.core.InternityException;
 import internity.core.Internship;
