04a29772d6ec9bbbb1c52abb5dd93e14d964be6b
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-22 20:09:01 +0800

    Fix checkstyle error

--- src/test/java/internity/commands/UsernameCommandTest.java
+++ src/test/java/internity/commands/UsernameCommandTest.java
@@ -1,6 +1,7 @@
 package internity.commands;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
