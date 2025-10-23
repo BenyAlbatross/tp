19833dd8a7d82fb09abb5d8df39cf1e3dc32b494
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-23 23:03:08 +0800

    Add JavaDocs to CommandFactory

--- src/main/java/internity/commands/CommandFactory.java
+++ src/main/java/internity/commands/CommandFactory.java
@@ -3,6 +3,26 @@ package internity.commands;
 import internity.cli.ArgumentParser;
 import internity.core.InternityException;
 
+/**
+ * A factory class responsible for creating {@link Command} objects
+ * based on the user input command word and its arguments.
+ * <p>
+ * The {@code CommandFactory} delegates argument parsing to the
+ * {@link ArgumentParser} and returns the corresponding {@link Command}
+ * subclass instance for execution. If the provided command word does not
+ * match any recognized command, an {@link InternityException} is thrown.
+ * </p>
+ *
+ * <p>Supported commands include:</p>
+ * <ul>
+ *   <li>{@link AddCommand}</li>
+ *   <li>{@link DeleteCommand}</li>
+ *   <li>{@link UpdateCommand}</li>
+ *   <li>{@link ListCommand}</li>
+ *   <li>{@link UsernameCommand}</li>
+ *   <li>{@link ExitCommand}</li>
+ * </ul>
+ */
 public class CommandFactory {
     public Command createCommand(String commandWord, String args) throws InternityException {
         switch (commandWord) {
