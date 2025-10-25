8fdd696eebea72be60a16ff3b07c9dbc619c4966
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-25 20:51:34 +0800

    Update DashboardUI.java

--- src/main/java/internity/ui/DashboardUI.java
+++ src/main/java/internity/ui/DashboardUI.java
@@ -1,5 +1,20 @@
 package internity.ui;
 
-public class DashboardUI {
+import internity.core.InternshipList;
 
+public class DashboardUI {
+    public DashboardUI() {
+
+    }
+
+    public void printUser() {
+        String user = InternshipList.getUsername();
+    }
+
+    public void printInternshipCount() {
+        System.out.println("Total Internships: " + InternshipList.size());
+    }
+
+    public void printStatusOverview() {
+    }
 }
