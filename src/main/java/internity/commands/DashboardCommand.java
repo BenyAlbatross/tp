8507512aeaa312342c4cc3bc04813748c092cc0e
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-26 01:41:57 +0800

    Update DashboardCommand.java

--- src/main/java/internity/commands/DashboardCommand.java
+++ src/main/java/internity/commands/DashboardCommand.java
@@ -1,9 +1,12 @@
 package internity.commands;
 
+import internity.core.InternityException;
+import internity.ui.DashboardUI;
+
 public class DashboardCommand extends Command {
     @Override
-    public void execute() {
-
+    public void execute() throws InternityException {
+        DashboardUI.printDashboard();
     }
 
     @Override
