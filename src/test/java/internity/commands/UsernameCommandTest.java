8a9873bd631e66dcd73e016c298b1b209f4f34a0
lukeai-tan <lukeaidantan1612@gmail.com>
2025-10-27 17:30:48 +0800

    Add JUnit tests for UserCommand

--- src/test/java/internity/commands/UsernameCommandTest.java
+++ src/test/java/internity/commands/UsernameCommandTest.java
@@ -2,6 +2,7 @@ package internity.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,7 @@ class UsernameCommandTest {
 
     @Test
     void execute_shouldSetUsernameInInternshipList() {
-        String expectedUsername = "Rob Banks";
+        String expectedUsername = "Jim Halpert";
         UsernameCommand command = new UsernameCommand(expectedUsername);
         command.execute();
         assertEquals(expectedUsername, InternshipList.getUsername(),
@@ -25,9 +26,29 @@ class UsernameCommandTest {
     }
 
     @Test
+    void execute_updatesSystemEvenAfterTrimmedInput() {
+        String username = "  Asian Jim  ";
+        UsernameCommand command = new UsernameCommand(username);
+        command.execute();
+        assertEquals(username, InternshipList.getUsername(),
+                "Username should be set exactly as provided, even with extra spaces");
+    }
+
+    @Test
     void isExit_shouldReturnFalse() {
         UsernameCommand command = new UsernameCommand("Dwight Schrute");
         assertFalse(command.isExit(),
                 "UsernameCommand should not be an exit command");
+    }
+
+    @Test
+    void constructor_nullUsername_throwsAssertionError() {
+        assertThrows(AssertionError.class, () -> new UsernameCommand(null));
+    }
+
+    @Test
+    void constructor_blankUsername_throwsAssertionError() {
+        assertThrows(AssertionError.class, () -> new UsernameCommand(""));
+        assertThrows(AssertionError.class, () -> new UsernameCommand("   "));
     }
 }
